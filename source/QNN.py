cab25c38141789c917dafb8a047c266adf4b394b
Miquel Romero <miquel.emt2@gmail.com>
2021-11-25 13:23:07 +0100

    torch classes

---
+++ source/QNN.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import os
+
+class Q_LNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        pass
+
+
+class Q_CNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        pass
